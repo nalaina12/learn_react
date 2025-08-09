87d7176ca87d44f78cfe378eaa5057590f5c297e
Nalaina <nofonalaina@gmail.com>
2025-08-09 15:47:02 +0300

    comments in react

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,8 +5,10 @@ function App() {
     <Fragment>
       <h1>Hello World</h1>
       <h2>Learing to code with react</h2>
+      {/* comment inside the return */}
     </Fragment>
   )
 }
 
 export default App
+// comment outside the return
