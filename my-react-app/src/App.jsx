399284990e588f17f94b9b9b6304820e26d031df
Nalaina <nofonalaina@gmail.com>
2025-08-09 16:37:33 +0300

    first using js variable

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,11 @@
 import { Fragment } from "react"
 function App() {
+  const myElement = <h2>Learing to code with react</h2>
 
   return (
     <Fragment>
       <h1>Hello World</h1>
-      <h2>Learing to code with react</h2>
+      {myElement}
       {/* comment inside the return */}
     </Fragment>
   )
