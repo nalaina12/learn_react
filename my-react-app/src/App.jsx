2f1d0bd936f156cf873ec91408da531786ad61dd
Nalaina <nofonalaina@gmail.com>
2025-08-11 21:03:53 +0300

    Render a list Using JSX

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,24 @@
 import { Fragment } from "react"
 function App() {
   const myElement = <h2>Learing to code with react</h2>
-
+  // Render a list using jsx
+  const users = [
+    { id: 1, name: "John Doe" , role: "Admin"},
+    { id: 2, name: "Jane Smith", role: "Editor" },
+    { id: 3, name: "Alice Johnson", role: "Viewer" }
+  ]
   return (
     <Fragment>
       <h1>Hello World</h1>
       {myElement}
+      <p>The users list:</p>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.name} - {user.role}
+          </li>
+        ))}
+      </ul>
       {/* comment inside the return */}
     </Fragment>
   )
