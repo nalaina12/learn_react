3386ddcdb1ea32bc4834250d5a80918ec28a2468
Nalaina <nofonalaina@gmail.com>
2025-08-13 21:15:39 +0300

    initial commit to my to Do app

---
+++ toDo_app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const subject = 'React';
+function App() {
+  return (
+    <>
+      <h1>Hello {subject}</h1>
+      <button type='button' className='primary'>Click Me</button>
+    </>
+  )
+}
+
+export default App
