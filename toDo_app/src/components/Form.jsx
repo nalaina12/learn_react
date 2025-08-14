1fa4ee1eda13badcd49ba558cbe0b75acb27fa06
Nalaina <nofonalaina@gmail.com>
2025-08-14 22:52:23 +0300

    reading state

--- toDo_app/src/components/Form.jsx
+++ toDo_app/src/components/Form.jsx
@@ -5,14 +5,14 @@ function Form(props){
 
     function handleSubmit(event) {
         event.preventDefault();
-         props.onSubmit("Task added");
+         props.onSubmit(name);
     }
     return (
         <form onSubmit={handleSubmit}>
             <h2 className="label-wrapper">
             <label htmlFor="new-todo-input" className="label_lg">What needs to be done?</label>
             </h2>
-            <input type="text" name="text" id="new-todo-input" className="input input_lg" autoComplete="off" />
+            <input type="text" name="text" id="new-todo-input" className="input input_lg" autoComplete="off" value={name}/>
             <button type="submit" className="btn btn_primary btn_lg">Add</button>
         </form>
     );
