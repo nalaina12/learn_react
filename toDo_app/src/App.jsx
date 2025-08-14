fe76e82499ac050823a24d18153572c05541dea9
Nalaina <nofonalaina@gmail.com>
2025-08-14 21:30:31 +0300

    import the components

--- toDo_app/src/App.jsx
+++ toDo_app/src/App.jsx
@@ -1,3 +1,5 @@
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
 function App(props) {
@@ -7,29 +9,11 @@ function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>To-Do List</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label_lg">What needs to be done?</label>
-        </h2>
-        <input type="text" name="text" id="new-todo-input" className="input input_lg" autoComplete="off" />
-        <button type="submit" className="btn btn_primary btn_lg">Add</button>
-      </form>
+      <Form />
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show</span>
-          <span>All</span>
-          <span className="visually-hidden">Tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show</span>
-          <span>Active</span>
-          <span className="visually-hidden">Tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show</span>
-          <span>Completed</span>
-          <span className="visually-hidden">Tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul role="list" className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
