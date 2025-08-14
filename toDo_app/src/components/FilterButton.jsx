622b604225dc1b97bfe3602e3e7e5443794ac31b
Nalaina <nofonalaina@gmail.com>
2025-08-14 21:25:59 +0300

    create Form and FilterButton components

---
+++ toDo_app/src/components/FilterButton.jsx
@@ -0,0 +1,10 @@
+function FilterButton(){
+    return (
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+            <span className="visually-hidden">Show</span>
+            <span>All</span>
+            <span className="visually-hidden">Tasks</span>
+        </button>        
+    );
+}
+export default FilterButton;
---
+++ toDo_app/src/components/Form.jsx
@@ -0,0 +1,12 @@
+function Form(){
+    return (
+        <form>
+            <h2 className="label-wrapper">
+            <label htmlFor="new-todo-input" className="label_lg">What needs to be done?</label>
+            </h2>
+            <input type="text" name="text" id="new-todo-input" className="input input_lg" autoComplete="off" />
+            <button type="submit" className="btn btn_primary btn_lg">Add</button>
+        </form>
+    );
+}
+export default Form;
