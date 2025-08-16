8b20902cd950dadb2699f9b85a6619cd11c52f4d
Nalaina <nofonalaina@gmail.com>
2025-08-16 16:30:02 +0300

    filtering tasks

--- toDo_app/src/App.jsx
+++ toDo_app/src/App.jsx
@@ -3,8 +3,15 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
+const FILTER_MAP = {
+  all: () => true,
+  active: (task) => !task.completed,
+  completed: (task) => task.completed,
+};
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("all");
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task) =>
       task.id === id ? { ...task, completed: !task.completed } : task
@@ -22,8 +29,11 @@ function App(props) {
     setTasks(editTaskList);
   }
 
-  const taskList = tasks?.map((task) => (
+  const taskList = tasks.filter(FILTER_MAP[filter]).map((task) => (
     <Todo key={task.id} name={task.name} id={task.id} completed={task.completed} toggleTaskCompleted={toggleTaskCompleted} deleteTask={deleteTask} editTask={editTask} />
+  ));
+  const filterList = Object.keys(FILTER_MAP).map((name) => (
+    <FilterButton key={name} name={name} isPressed={name === filter} setFilter={setFilter} />
   ));
   function addTask(name) {
     const newTask = { id: "id" + Date.now(), name, completed: false };
@@ -38,9 +48,7 @@ function App(props) {
       <h1>To-Do List</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul role="list" className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
